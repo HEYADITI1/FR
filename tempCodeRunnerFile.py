42c8dbaabbe3a09ea8bc86fc10144487d6edbd30
Aditi <aditi88599@gmail.com>
2024-02-01 18:19:50 +0530

    attendance done

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+title_lbl=Label(bg_img,text="FACE RECOGNITION ATTENDANCE SYSTEM SOFTWARE",font=("times new roman",35,"bold"),bg="white",fg="red")
+        title_lbl.place(x=0,y=0,width=1530,height=45)
