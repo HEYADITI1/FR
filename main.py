1a7669e88094a49aa7d7950559d801b968235b7b
Kopal05 <pareekkopal41@gmail.com>
2024-02-01 16:54:18 +0530

    made changes in main file

--- main.py
+++ main.py
@@ -114,10 +114,10 @@ class Face_Recognition_System:
         img9=img9.resize((220,220),Image.ANTIALIAS)
         self.photoimg9=ImageTk.PhotoImage(img9)
 
-        b1=Button(bg_img,image=self.photoimg9,cursor="hand2")
+        b1=Button(bg_img,image=self.photoimg9,cursor="hand2",command=self.open_img)
         b1.place(x=500,y=380,width=220,height=220)
 
-        b1_1=Button(bg_img,text="Photos",cursor="hand2",font=("times new roman",15,"bold"),bg="darkblue",fg="white")
+        b1_1=Button(bg_img,text="Photos",cursor="hand2",command=self.open_img,font=("times new roman",15,"bold"),bg="darkblue",fg="white")
         b1_1.place(x=500,y=580,width=220,height=40)
 
 
@@ -143,6 +143,10 @@ class Face_Recognition_System:
 
         b1_1=Button(bg_img,text="Exit",cursor="hand2",font=("times new roman",15,"bold"),bg="darkblue",fg="white")
         b1_1.place(x=1100,y=580,width=220,height=40)
+
+    def open_img(self):
+        os.startfile("data")
+
 
 
 
